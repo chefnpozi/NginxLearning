14edded4c6f493fe92bfb3170a069d7837c94a25
fengh11 <fengh11@chinatelecom.cn>
2023-03-10 14:57:24 +0800

    vts init

--- nginx-1.22.1/config.sh
+++ nginx-1.22.1/config.sh
@@ -2,7 +2,9 @@
                                             --with-http_realip_module --with-http_sub_module \
                                             --with-http_ssl_module --with-http_ssl_module \
 											--with-pcre-jit \
-                                            --with-openssl=/home/chezi/local/our_code/TOOL/openssl-1.0.1j \
+                                            --with-openssl=${project_name}/TOOL/openssl-1.0.1j \
                                             --with-debug \
                                             --with-cc-opt="-Werror -W -O0" \
-                                            --add-module=/home/chezi/local/our_code/TOOL/nginx-rtmp-module; make && make install
+                                            --add-module=${project_name}/TOOL/nginx-rtmp-module \
+                                            --add-module=${project_name}/TOOL/nginx-module-vts; make && make install
+                                            
